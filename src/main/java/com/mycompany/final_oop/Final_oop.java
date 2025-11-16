81dcde72f39728cf3342e7af137391283e0cfe3f
QE <qe@MacBook-Pro-khxng-QE.local>
2025-11-16 17:24:27 +0700

    first commit

---
+++ src/main/java/com/mycompany/final_oop/Final_oop.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.final_oop;
+
+/**
+ *
+ * @author qe
+ */
+public class Final_oop {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
